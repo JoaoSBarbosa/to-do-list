c04ecc85ed9e1fa3c806d546aa64215f211f8661
João Barbosa <contato.jsbarbosa@gmail.com>
2023-05-15 20:53:21 -0300

    Props Todo

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import Todo from "./components/Todo";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -28,16 +29,7 @@ function App() {
       <h1>Lista de Tarefas</h1>
       <div className="todo-list">
         {todos.map((todo) => (
-          <div className="todo">
-            <div className="content">
-              <p className="text">{todo.text}</p>
-              <p className="category">({todo.category})</p>
-              <div className="action-buttons">
-                <button className="complete">Completar</button>
-                <button className="delete">X</button>
-              </div>
-            </div>
-          </div>
+          <Todo todo={todo} />
         ))}
       </div>
     </div>
---
+++ to-do/src/components/Todo.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Todo = ({ todo }) => {
+  return (
+    <div className="todo">
+      <div className="content">
+        <p className="text">{todo.text}</p>
+        <p className="category">({todo.category})</p>
+        <div className="action-buttons">
+          <button className="complete">Completar</button>
+          <button className="delete">X</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
