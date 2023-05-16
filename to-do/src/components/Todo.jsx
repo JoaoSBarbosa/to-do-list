3c4f1259e7298b94ec0ed9e06eb70ba9a6dae082
João Barbosa <contato.jsbarbosa@gmail.com>
2023-05-16 20:15:29 -0300

    Função 'completeTodo' para finalizar ou abrir uma tarefa todo

--- to-do/src/components/Todo.jsx
+++ to-do/src/components/Todo.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 
-const Todo = ({ todo, removeTodo }) => {
+const Todo = ({ todo, removeTodo, completeTodo }) => {
   return (
-    <div className="todo">
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
       <div className="content">
         <p className="text">{todo.text}</p>
         <p className="category">({todo.category})</p>
       </div>
       <div className="action-buttons">
-        <button className="complete">Completar</button>
+        <button className="complete" onClick={() => completeTodo(todo.id)}>
+          Completar
+        </button>
         <button className="delete" onClick={() => removeTodo(todo.id)}>
           X
         </button>
