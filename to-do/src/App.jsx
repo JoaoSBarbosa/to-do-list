22494cfb50b91ca1b3a51cf8a7a3b5da676c7189
João Barbosa <contato.jsbarbosa@gmail.com>
2023-05-16 20:27:42 -0300

    Função 'Search' para realizar busca em tempo real

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Todo from "./components/Todo";
 import TodoForms from "./components/TodoForms";
+import Search from "./components/Search";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -24,7 +25,7 @@ function App() {
       isCompleted: false,
     },
   ]);
-
+  const [search, setSearch] = useState("");
   const addTodo = (text, category) => {
     const newTodos = [
       ...todos,
@@ -56,15 +57,20 @@ function App() {
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
+      <Search search={search} setSearch={setSearch} />
       <div className="todo-list">
-        {todos.map((todo) => (
-          <Todo
-            key={todo.id}
-            todo={todo}
-            removeTodo={removeTodo}
-            completeTodo={completeTodo}
-          />
-        ))}
+        {todos
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              completeTodo={completeTodo}
+            />
+          ))}
       </div>
       <TodoForms addTodo={addTodo} />
     </div>
