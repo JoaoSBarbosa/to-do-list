2d0433d02ca3d399f82ab9f50d26fe3cc82efd2f
João Barbosa <contato.jsbarbosa@gmail.com>
2023-05-15 21:38:15 -0300

    Manipulando os dados com s função: heandleSubmit e eventos

--- to-do/src/components/TodoForms.jsx
+++ to-do/src/components/TodoForms.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import { useState } from "react";
 
 const TodoForms = () => {
+  const [value, setValue] = useState("");
+  const [category, setCategory] = useState("");
+  const heandleSubmit = (event) => {
+    event.preventDefault();
+    if (!value || !category) return;
+
+    setCategory("");
+    setValue("");
+  };
   return (
     <div className="todo-form">
       <h2>Criar Tarefa</h2>
-      <form>
-        <input type="text" placeholder="Digite o titulo" />
-        <select>
+      <form onSubmit={heandleSubmit}>
+        <input
+          value={value}
+          type="text"
+          placeholder="Digite o titulo"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
           <option value="">Selecione uma categoria</option>
           <option value="Trabalho">Trabalho</option>
           <option value="Estudos">Estudos</option>
