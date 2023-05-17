9f1ed7eac1a9613b1bf012d1d03e1cd2203aae92
João Barbosa <contato.jsbarbosa@gmail.com>
2023-05-16 21:51:04 -0300

    Função 'Filter' para filtrar tarefas abertas, completas e todas

--- to-do/src/components/Filter.jsx
+++ to-do/src/components/Filter.jsx
@@ -1,14 +1,14 @@
-const Filter = () => {
+const Filter = ({ filter, setFilter }) => {
   return (
     <div className="filter">
       <h2>Filtrar:</h2>
       <div className="filter-options">
         <div>
           <p>Status:</p>
-          <select>
+          <select value={filter} onChange={(e) => setFilter(e.target.value)}>
             <option value="All">Todos</option>
-            <option value="Completed">Completas</option>
-            <option value="Incomplete">Incompletas</option>
+            <option value="completed">Completas</option>
+            <option value="incomplete">Incompletas</option>
           </select>
         </div>
         <div>
